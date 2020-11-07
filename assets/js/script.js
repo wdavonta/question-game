881b1ece22d24f1ad372d7c5570af0db5b444545
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-11-07 11:33:29 -0500

    added button html added track score formula

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,87 @@
+var questionArray = [
+{
 
 
-Question: how many championship michael jordand have
-Choices: [
+    question: "How many championship Michael Jordan have?",
+    choices: [
 
-1
-8
-4
-6
+    "1",
+    "8",
+    "4",
+    "6"
+    ],
+    answer: "4"
+},
+{
+
+
+    question: "What team Kobe Bryant was drafted to first?",
+    choices: [
+
+    "Los Angles Lakers",
+    "Charlotte Hornets",
+    "Orlando Magic",
+    "Philedphia 76ers"
+    ],
+    answer: "Charlotte Hornets"
+},
+{
+
+
+    question: "What year did Dwayne Wade win his first championship?", 
+    choices: [
+
+    "2002",
+    "2008",
+    "2010",
+    "2006"
+    ],
+    answer: "2006"
+},
+{
+
+
+    question: "When did the NBA started?",
+    choices: [
+
+    "June 6th, 1946",
+    "August 15th, 1955",
+    "May 9th, 1960",
+    "October 5th, 1991",
+    ],
+    answer: "June 6th, 1946"
+}
 ]
-Answer:"4"
+
+
+//var timmer
+var timer= -1;
 
 
 
 
-Question: What team Kobe Bryant was drafted to first?
-Choices: [
+var count= 75;
 
-Los Angles Lakers
-Charlotte Hornets
-Orlando Magic
-Houston Rockets
-]
-Answer:"2"
+document.getElementById('count)').innerHTML=count;
+count--;
 
 
+// keep track of score
+var score = 0;
 
-Question: When did Dwayne Wade won his first championship?
-Choices: [
-2002
-2008
-2010
-2006
-]
-Answer:"4"
+for (var i = 0; i < questionsArray.length; i++){
+    var userAnswer = confirm(questionsArray[i].question);
 
-Question: When did the NBA started?
-Choices: [
-June 6th, 1946
-August 15th, 1955
-May 9th, 1960
-October 5th, 1991
-]
-Answer:"1"
+    // TODO: Check the user's answer against the correct answer
+    if (userAnswer === answerArray[i]){
+        // TODO: Alert the user if they are correct or wrong. Increment the score accordingly
+                score++;
+                alert("Correct!");
+            } else {
+                alert("Wrong!");
+            }
+        }
+    
+
+        //alert the user with the final score
+alert("Finished! Your score was " + score + " out of " + questionsArray.length);
