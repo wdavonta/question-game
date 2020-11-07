9ab7b4983a60eff702ed9876f687a34b1345c153
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-11-06 22:23:42 -0500

    added questions to js

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+
+Question: how many championship michael jordand have
+Choices: [
+
+1
+8
+4
+6
+]
+Answer:"4"
+
+
+
+
+Question: What team Kobe Bryant was drafted to first?
+Choices: [
+
+Los Angles Lakers
+Charlotte Hornets
+Orlando Magic
+Houston Rockets
+]
+Answer:"2"
+
+
+
+Question: When did Dwayne Wade won his first championship?
+Choices: [
+2002
+2008
+2010
+2006
+]
+Answer:"4"
+
+Question: When did the NBA started?
+Choices: [
+June 6th, 1946
+August 15th, 1955
+May 9th, 1960
+October 5th, 1991
+]
+Answer:"1"
